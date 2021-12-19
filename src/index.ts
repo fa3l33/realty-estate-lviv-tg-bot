8460d03d00eea7b45a072298ee445332d2c02dab
ychernenko <fa3l33.yc@gmail.com>
2021-12-19 15:36:12 +0300

    Add tinyrom <-> bot session adapter.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
+import 'reflect-metadata';
 import { config } from 'dotenv';
 import { Bot, lazySession } from 'grammy'
+import { createConnection } from 'typeorm';
 import { registerCommands, setCommands } from './bll/tg/command';
 import getMenus from './bll/tg/menu';
 import { initialize, SessionContextFlavor } from './bll/tg/session-context';
+import { User } from './dal/model/tg/user';
+import { TypeOrmAdapter } from './dal/user-storage-adapter';
 
-config();
+async function bootstrap() {
+  // create global MySql connection
+  const connection = await createConnection();
 
-const bot = new Bot<SessionContextFlavor>(process.env.TG_BOT_SECRET_KEY as string);
+  // load .env
+  config();
+  
+  const bot = new Bot<SessionContextFlavor>(process.env.TG_BOT_SECRET_KEY as string);
+  
+  bot.use(lazySession({
+     initial: initialize,
+     storage: new TypeOrmAdapter({ repository: connection.getRepository(User) })
+   }));
 
-bot.use(lazySession({ initial: initialize }));
-bot.use(getMenus());
-setCommands(bot);
-registerCommands(bot);
+  bot.use(getMenus());
+  setCommands(bot);
+  registerCommands(bot);
+  
+  bot.catch((value) => {
+    console.log(value);  
+  });
+  
+  bot.start()  
+}
 
-bot.catch((value) => {
-  console.log(value);  
-});
-
-bot.start()
-
+bootstrap();
