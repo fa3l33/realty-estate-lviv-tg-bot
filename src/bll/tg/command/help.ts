9cb3a14ff5a6cb5371b2ca105a8f8193538f660e
ychernenko <fa3l33.yc@gmail.com>
2021-12-25 14:21:57 +0300

    Add help text.

--- src/bll/tg/command/help.ts
+++ src/bll/tg/command/help.ts
@@ -1,5 +1,15 @@
+import TextUtils from '../../../common/text-utils';
+import config from '../../../config';
 import { SessionContextFlavor } from './../session-context';
 
 export default function help(ctx: SessionContextFlavor) {
-     ctx.reply("Help Command should return all command description and bot description.");
+     ctx.reply(`${ TextUtils.toBold(TextUtils.toUnderline('REALTY GROUP'))}\n\n`
+     + `${ TextUtils.toBold('Адреса')}\n`
+     + `м. Херсон, вул. Театральна, 17`, 
+     {
+          parse_mode: 'HTML'
+     });
+
+     let chatId = Number(ctx.chat?.id);
+     ctx.api.sendContact(chatId, config.realtyGroup.MANAGER_PHONE as string, 'Realty Group');
 }
