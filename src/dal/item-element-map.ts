77327481968d185977edcca449faf86368189ca4
ychernenko <fa3l33.yc@gmail.com>
2021-12-26 16:46:07 +0300

    Add ItemElement mapping.

---
+++ src/dal/item-element-map.ts
@@ -0,0 +1,21 @@
+import { ItemElementType } from './enums/item-elemnt-type';
+
+export const ItemElementMap: Map<ItemElementType, string> = new Map<ItemElementType, string>([
+    [ItemElementType.IS_VISIBLE_ON_SITE, '299a6fa4-9e97-45c2-aac4-3b66fc41f6ce'],
+    [ItemElementType.AGENT_ID, '87a7afdb-f509-45a6-b34c-9504dbaebb31'],
+    [ItemElementType.TYPE, 'f45640e1-4b14-4141-9bef-e15a89c200fa'],
+    [ItemElementType.RENT_PERIOD, '435aaa96-d51e-433b-9242-19e5b104acdd'],
+    [ItemElementType.PRICE_USD, 'cfb6bd72-74d0-4bba-b44b-2ba2996f8fba'],
+    [ItemElementType.RENT_PRICE_UA, '5b8f428f-7a25-48e8-9ec0-24e4df44d339'],
+    [ItemElementType.DISTRICT, '38a43728-a630-4ab0-99f9-f9dc7f3567cd'],
+    [ItemElementType.FLOOR, '95d393a2-dfb1-4f88-becf-3b158834498a'],
+    [ItemElementType.HOUSE_TOTAL_FLOORS, '993f99bf-e7d1-416f-ad6e-db9a2c16e42b'],
+    [ItemElementType.ROOMS_COUNT, 'c92e7d11-3293-4b42-be42-4698f64bb7a0'],
+    [ItemElementType.TOTAL_AREA, 'f3217a7e-4837-4995-b20f-533c30d08f1c'],
+    [ItemElementType.LIVE_AREA, 'c52b0560-ae2a-469c-b859-3d24be37fa06'],
+    [ItemElementType.KITCHEN_AREA, '82faf2b1-e9e0-4b46-b659-90c9564694b6'],
+    [ItemElementType.LAND_HOUSE_AREA, 'c655d697-685b-4126-9056-340c45e80545'],
+    [ItemElementType.DESCRIPTION, '2966bf6d-2e4c-4377-b99b-033da85cef02'],
+    [ItemElementType.MAIN_PHOTO, '26fc8979-6eb3-4c3e-806f-24b5e7d66e96'],
+    [ItemElementType.GALLERY_FOLDER_NAME, '70d2c2da-292f-47bd-960c-78c61920c086'],
+]);
