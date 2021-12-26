c5903375d54846117e5ea43186b81846f91bc940
ychernenko <fa3l33.yc@gmail.com>
2021-12-26 16:45:53 +0300

    Add ItemElement enum.

---
+++ src/dal/enums/item-elemnt-type.ts
@@ -0,0 +1,20 @@
+export enum ItemElementType {
+    IS_VISIBLE_ON_SITE,
+    AGENT_ID,
+    // prodazha, pokupka, sdam, snimu
+    TYPE,
+    RENT_PERIOD,
+    PRICE_USD,
+    RENT_PRICE_UA,
+    DISTRICT,
+    FLOOR,
+    HOUSE_TOTAL_FLOORS,
+    ROOMS_COUNT,
+    TOTAL_AREA,
+    LIVE_AREA,
+    KITCHEN_AREA,
+    LAND_HOUSE_AREA,
+    DESCRIPTION,
+    MAIN_PHOTO,
+    GALLERY_FOLDER_NAME
+}
