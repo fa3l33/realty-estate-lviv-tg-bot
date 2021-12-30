6fb673f553c5f73d94ac7bc7cc08be9eceaf22d3
ychernenko <fa3l33.yc@gmail.com>
2021-12-30 11:19:11 +0300

    Add db initializer,

---
+++ src/db-initializer.ts
@@ -0,0 +1,27 @@
+import { Item } from './dal/model/rg_zoo/item';
+import { CategoryItem } from './dal/model/rg_zoo/category-item';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+import config from "./config";
+import { Category } from './dal/model/rg_zoo/category';
+import { User } from './dal/model/tg/user';
+
+export default function initializeDataBase(): Promise<Connection> {
+
+    if (config.isProduction === 'TRUE')
+    {
+        return createConnection({
+            "type": config.db_type,
+            "host": config.db_host,
+            "port": config.db_port,
+            "username": config.db_username,
+            "password": config.db_password,
+            "database": config.db_database,
+            "logging": true,
+            synchronize: false,
+            entities: [User, Category, CategoryItem, Item]
+            } as ConnectionOptions);
+    } else {
+        // read connection option from file
+        return createConnection();
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,70 @@
-import 'reflect-metadata';
-import config from './config';
-import { Bot, Keyboard, lazySession } from 'grammy'
-import { createConnection } from 'typeorm';
-import { registerCommands, setCommands } from './bll/tg/command';
-import getMenus from './bll/tg/menu';
-import { initialize, SessionContextFlavor } from './bll/tg/session-context';
-import { User } from './dal/model/tg/user';
-import { TypeOrmAdapter } from './dal/user-storage-adapter';
-import { registerMenuCallbacks } from './bll/tg/command/template-one';
-
+import "reflect-metadata";
+import config from "./config";
+import { Bot, Keyboard, lazySession } from "grammy";
+import { getRepository } from "typeorm";
+import { registerCommands, setCommands } from "./bll/tg/command";
+import getMenus from "./bll/tg/menu";
+import { initialize, SessionContextFlavor } from "./bll/tg/session-context";
+import { User } from "./dal/model/tg/user";
+import { TypeOrmAdapter } from "./dal/user-storage-adapter";
+import { registerMenuCallbacks } from "./bll/tg/command/template-one";
+import initializeDataBase from "./db-initializer";
+ 
 async function bootstrap() {
-  // create global MySql connection
-  const connection = await createConnection();  
-  
-  const bot = new Bot<SessionContextFlavor>(config.telegram.BOT_SECRET_KEY as string);
-  
-  bot.use(lazySession({
-     initial: initialize,
-     storage: new TypeOrmAdapter({ repository: connection.getRepository(User) })
-   }));
+    // create global MySql connection
+    await initializeDataBase();
 
-  bot.use(getMenus());
-  setCommands(bot);
-  registerCommands(bot);
+    const bot = new Bot<SessionContextFlavor>(
+      config.telegram.BOT_SECRET_KEY as string
+    );
 
-  registerMenuCallbacks(bot);
-  
-  bot.on('message:contact', ctx => {
-    ctx.reply('thx', 
-    {
-      reply_markup: {remove_keyboard: true }
+    bot.use(
+      lazySession({
+        initial: initialize,
+        storage: new TypeOrmAdapter({ repository: getRepository(User) }),
+      })
+    );
+
+    bot.use(getMenus());
+    setCommands(bot);
+    registerCommands(bot);
+
+    registerMenuCallbacks(bot);
+
+    bot.on("message:contact", (ctx) => {
+      ctx.reply("thx", {
+        reply_markup: { remove_keyboard: true },
+      });
     });
-  });
 
-  bot.on('callback_query:data', ctx => {    
-    ctx.answerCallbackQuery();
-    console.log(ctx.callbackQuery.data);    
+    bot.on("callback_query:data", (ctx) => {
+      ctx.answerCallbackQuery();
+      console.log(ctx.callbackQuery.data);
 
       ctx.reply(`Оберіть спосіб для зворотнього зв'язку.`, {
-        reply_markup: 
-        {
+        reply_markup: {
           one_time_keyboard: true,
-          keyboard: new Keyboard().requestContact('Зателефонуйте мені\n (поділитися номером телефону)').row().text('Напишіть мені.').build(),
+          keyboard: new Keyboard()
+            .requestContact(
+              "Зателефонуйте мені\n (поділитися номером телефону)"
+            )
+            .row()
+            .text("Напишіть мені.")
+            .build(),
           resize_keyboard: true,
           selective: true,
-          input_field_placeholder: `Оберіть спосіб для зворотнього зв'язку.`
-        }
+          input_field_placeholder: `Оберіть спосіб для зворотнього зв'язку.`,
+        },
       });
-  });
-  
-  bot.catch((value) => {
-    console.log(value);  
-  });
-  
-  bot.start()  
+    });
+
+    bot.catch((value) => {
+      console.log(value);
+    });
+
+    bot.start();
 }
 
 bootstrap();
+
+export default bootstrap;
