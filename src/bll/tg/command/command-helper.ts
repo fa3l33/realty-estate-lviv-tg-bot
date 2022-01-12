3cccc5950aee40f4fc62a6ffe8603f4f97a910e7
ychernenko <fa3l33.yc@gmail.com>
2022-01-12 12:03:42 +0300

    Remove unused commands.

--- src/bll/tg/command/command-helper.ts
+++ src/bll/tg/command/command-helper.ts
@@ -4,7 +4,6 @@ import { SessionContextFlavor } from "../session-context";
 import cancel from "./cancel";
 import help from "./help";
 import start from "./start";
-import { templateOne } from "./template-one";
 
 export default abstract class CommandHelper {
   public static init(bot: Bot<SessionContextFlavor>) {
@@ -12,18 +11,15 @@ export default abstract class CommandHelper {
     
     bot.api.setMyCommands([
         { command: "start", description: "Початок" },
-        { command: "help", description: "Допомога" },
-        { command: "cancel", description: "Призупинити роботу бота" },
         { command: "filter", description: "Налаштування Фільтру" },
-        { command: "message", description: "Вигляд Повідомлення" },
-        { command: "filter2", description: "Налаштування Фільтру (menu)" },
+        { command: "help", description: "Допомога" },
+        { command: "cancel", description: "Призупинити роботу бота" }
     ]);
     
     bot.command("start", start);
     bot.command("filter", (ctx) => menuComposer.sendDefaultMenu(ctx));
     bot.command("help", help);
     bot.command("cancel", cancel);
-    bot.command("message", templateOne);
 
     menuComposer.addListener(bot);
   }
