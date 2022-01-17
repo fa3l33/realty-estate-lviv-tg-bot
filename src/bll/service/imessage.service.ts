56f82e2892b2bee7f5421304084a7540d1999020
ychernenko <fa3l33.yc@gmail.com>
2022-01-17 10:50:53 +0300

    Add manager notification.

--- src/bll/service/imessage.service.ts
+++ src/bll/service/imessage.service.ts
@@ -1,6 +1,7 @@
 import { Item } from './../../dal/model/rg_zoo/item';
 
 export default interface IMessageService {
+    notifyInterestedIn(userId: number, isPhoneNotification: boolean): Promise<void>;
     postItem(item: Item, chatId: number, userId: number): void;
     postDetailedItem( userId: number, itemId: number) : Promise<void>;
 }
